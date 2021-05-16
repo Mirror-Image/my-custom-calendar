9fe43a47f9b739138b51fa46ea6d35ad0033d8ff
Maksim Borovik <borovik.maksim@gmail.com>
2021-05-16 11:32:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {Component} from "react";
+import Calendar from "./components/Calendar";
+
+class App extends Component {
+  state = {
+    date: null,
+  };
+
+  handleDateChange = date => this.setState({ date });
+
+  render() {
+    const { date } = this.state;
+
+    return (
+      <div>
+        {date && <p>Выбранная дата: {date.toLocaleDateString()}</p>}
+        <Calendar
+          onChange={this.handleDateChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
